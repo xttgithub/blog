42a006b90041aeddc54cc3a3d1de173050f0eb05
zhaoteng <xttyx666@163.com>
2019-05-06 19:00:12 +0800

    test

--- pub/js/blog.js
+++ pub/js/blog.js
@@ -7,13 +7,20 @@ define([],function(){
 	        location.href=tag+".html";
 	    }
     })
-    var locationStr = location.pathname;
-    var nowPage = locationStr.substring(locationStr.lastIndexOf('/') + 1,locationStr.lastIndexOf('.html'));
-    if(nowPage){
-        $("#pageList").find("#"+nowPage).addClass("active");
-    }else{
-        $("#pageList").find("#blogTemplate").addClass("active");
-    }
+    // var locationStr = location.pathname;
+    // var nowPage = locationStr.substring(locationStr.lastIndexOf('/') + 1,locationStr.lastIndexOf('.html'));
+    // if(nowPage){
+    //     $("#pageList").find("#"+nowPage).addClass("active");
+    // }else{
+    //     $("#pageList").find("#blogTemplate").addClass("active");
+    // }
+    var getQueryUrlString=function(a) {
+        var b = new RegExp("(^|&)" + a + "=([^&]*)(&|$)","i")
+          , c = window.location.search.substr(1).match(b);
+        return null  != c ? decodeURIComponent(c[2]) : null 
+    };
+    var nowPage=getQueryUrlString("tag");
+    $("#pageList").find("#"+nowPage).addClass("active");
     $.get('./' + nowPage +'.md', function(response, status, xhr){
         $("#markdownContent").html(marked(response));
     });
