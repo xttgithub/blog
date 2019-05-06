07ed3fa926835e515a1476880abd84b8a2492e52
zhaoteng <xttyx666@163.com>
2019-05-06 15:21:18 +0800

    博客模版修改

--- Gruntfile.js
+++ Gruntfile.js
@@ -77,10 +77,6 @@ module.exports = function(grunt) {
               {
                 match: 'timestamp',
                 replacement: '<%= new Date().getTime() %>'
-              },
-              {
-                match: 'fileAddress',
-                replacement: ''
               }
             ]
           },
